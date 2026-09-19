dd383edc7ae49ba7379491efead17435c0e17760
ttSamtopia <s-linden@live.se>
2026-09-19 11:45:54 +0200

    Add Book constructor

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const library = [];
+
+function Book (id, title, author, nPages, readStatus) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.nPages = nPages;
+    this.readStatus = readStatus;
+}
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author}, ${this.nPages} pages, ${this.readStatus ? "have read" : "not read yet"} (${this.id})`
+}
